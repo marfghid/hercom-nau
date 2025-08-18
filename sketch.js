7b68d27881028c59af3a643268f5e1fbeb54ee12
María Fernanda García <maria.garcia.h@ug.uchile.cl>
2025-08-18 15:26:00 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
 var d;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
+ createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
+   background(241,42,159);
 }
 
 function draw() {
-  d = random(50,100)
-  fill(255,random(55,200));
-  ellipse(mouseX, mouseY,d,d);
+  d = random (10,60);
+  
+  //cambiar a multicolor
+  colorMode (HSB, 360, 100, 100);
+  fill (random (0,359), 100, 100);
+  ellipse (mouseX, mouseY,d,d)
 }
 
 function keyPressed() {
